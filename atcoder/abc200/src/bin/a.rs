a51f9528c97aa7a7d19e6ea1ded51d9212ab5748
lufeee <exiga.mad@gmail.com>
2021-05-10 03:46:52 +0900

    [Add] Answer

--- atcoder/abc200/src/bin/a.rs
+++ atcoder/abc200/src/bin/a.rs
@@ -6,3 +6,16 @@ fn main() {
     println!("{}", if a > a / 100 * 100 { a / 100 + 1 } else { a / 100 })
     
 }
+
+/* Answer
+ *
+ * use proconio::input;
+ *
+ * fn main() {
+ *
+ *   input!{ n: u32 }
+ *   println!("{}", (n+99) / 100)
+ *
+ * }
+ * 
+ */
--- atcoder/abc200/src/bin/c.rs
+++ atcoder/abc200/src/bin/c.rs
@@ -21,3 +21,24 @@ fn main() {
     println!{"{}", ans}
     
 }
+
+/* Answer
+ *
+ * fn main() {
+ *   input!{
+ *     n: u32,
+ *     a: [i32, n],
+ *   }
+ *   let mut b: [i64, 200];
+ *   for i in 0..n {
+ *     b[a[i]%200]+=1;
+ *   }
+ *   let mut ans: i64;
+ *   for i in 0..200 {
+ *     ans+=(b[i]*(b[i]-1)/2)
+ *   }
+ *   println!("{}", ans)
+ *
+ * }
+ *
+ */
